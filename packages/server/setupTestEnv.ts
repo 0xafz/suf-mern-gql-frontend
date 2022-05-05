93e28e2fcdbe949c9198d657fd6e1323a4507a03
zkindest <zkindest5@gmail.com>
2022-05-05 13:45:38 +0530

    moved packages/server to https://github.com/zkindest/suf-mern-gql-backend

--- packages/server/setupTestEnv.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-import path from 'path'
-
-async function setup() {
-  dotenv.config({ path: path.resolve(__dirname, '.env.test') })
-}
-export default setup
--- packages/server/src/utils/schemaCleaner.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  virtuals: true,
-  transform: (_document: any, returnedObject: any) => {
-    delete returnedObject.__v
-    delete returnedObject.passwordHash
-  },
-}
