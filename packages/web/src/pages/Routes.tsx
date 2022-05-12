db07c9ae4d0c123608ebcb62d2b567e43fe39bd5
zkindest <zkindest5@gmail.com>
2022-05-12 13:23:52 +0530

    add Errorboundary, rename QuesList to Home

--- packages/web/src/pages/Routes.tsx
+++ packages/web/src/pages/Routes.tsx
@@ -6,8 +6,9 @@ import { lazy, Suspense } from 'react'
 import MainLayout from '~~/components/Layout/MainLayout'
 import LoadingSpinner from '~~/components/LoadingSpinner'
 import useMediaQuery from '~~/hooks/useMediaQuery'
+import ErrorBoundary from '~~/components/ErrorBoundary'
 
-const QuesList = lazy(() => import('./QuesList'))
+const Home = lazy(() => import('./Home'))
 const AllTags = lazy(() => import('./AllTags'))
 const AllUsers = lazy(() => import('./AllUsers'))
 const User = lazy(() => import('./User'))
@@ -21,72 +22,74 @@ const AppRoutes = () => {
   const isMdScreen = useMediaQuery('(min-width: 768px)')
 
   return (
-    <MainLayout>
-      <Suspense fallback={<LoadingSpinner />}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <QuesList />
-                {isMdScreen && <RightSidePanel />}
-              </>
-            }
-          />
-          <Route
-            path="/ask"
-            element={
-              user ? (
+    <ErrorBoundary>
+      <MainLayout>
+        <Suspense fallback={<LoadingSpinner />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
                 <>
-                  <AskQuestion />
+                  <Home />
                   {isMdScreen && <RightSidePanel />}
                 </>
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          />
-          <Route path="/tags" element={<AllTags />} />
-          <Route path="/users" element={<AllUsers />} />
-          <Route path="/user/:username" element={<User />} />
-          <Route
-            path="/questions/:quesId"
-            element={
-              <>
-                <Question />
-                {isMdScreen && <RightSidePanel />}
-              </>
-            }
-          ></Route>
-          <Route
-            path="/tags/:tagName"
-            element={
-              <>
-                <QuesList tagFilterActive={true} />
-                {isMdScreen && <RightSidePanel />}
-              </>
-            }
-          ></Route>
-          <Route
-            path="/search/:query"
-            element={
-              <>
-                <QuesList searchFilterActive={true} />
-                {isMdScreen && <RightSidePanel />}
-              </>
-            }
-          ></Route>
-          <Route
-            element={
-              <>
-                <NotFound />
-                {isMdScreen && <RightSidePanel />}
-              </>
-            }
-          ></Route>
-        </Routes>
-      </Suspense>
-    </MainLayout>
+              }
+            />
+            <Route
+              path="/ask"
+              element={
+                user ? (
+                  <>
+                    <AskQuestion />
+                    {isMdScreen && <RightSidePanel />}
+                  </>
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+            <Route path="/tags" element={<AllTags />} />
+            <Route path="/users" element={<AllUsers />} />
+            <Route path="/user/:username" element={<User />} />
+            <Route
+              path="/questions/:quesId"
+              element={
+                <>
+                  <Question />
+                  {isMdScreen && <RightSidePanel />}
+                </>
+              }
+            ></Route>
+            <Route
+              path="/tags/:tagName"
+              element={
+                <>
+                  <Home tagFilterActive={true} />
+                  {isMdScreen && <RightSidePanel />}
+                </>
+              }
+            ></Route>
+            <Route
+              path="/search/:query"
+              element={
+                <>
+                  <Home searchFilterActive={true} />
+                  {isMdScreen && <RightSidePanel />}
+                </>
+              }
+            ></Route>
+            <Route
+              element={
+                <>
+                  <NotFound />
+                  {isMdScreen && <RightSidePanel />}
+                </>
+              }
+            ></Route>
+          </Routes>
+        </Suspense>
+      </MainLayout>
+    </ErrorBoundary>
   )
 }
 
