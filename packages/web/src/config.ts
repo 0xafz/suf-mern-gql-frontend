9df14d04162b70c50e09795a35bcfd48441d39b3
zkindest <zkindest5@gmail.com>
2022-05-15 21:25:35 +0530

    add seo

---
+++ packages/web/src/config.ts
@@ -0,0 +1,12 @@
+export default {
+  title: `QAns App`,
+  shortName: `Afroz`,
+  subtitle: `Web Developer`,
+  description: `This is demo app which demostrates stackoverflow like SSR website.`,
+  author: `Afroz Mohammad`,
+  userName: `afroz_0000`, //default for all
+  twitterUserName: `afroz_0000`,
+  baseUrl: `https://suf-mern-gql-frontend.vercel.app/`,
+  image: `/logo256.png`,
+  codeUrl: `https://github.com/zkindest/suf-mern-gql-frontend`,
+}
