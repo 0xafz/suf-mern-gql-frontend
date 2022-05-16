2d0900a003fe0f41badaf62272ded694c3b6ed85
zkindest <zkindest5@gmail.com>
2022-05-16 13:08:39 +0530

    Wrap Link over a in `SortQuesTabGroup`

--- packages/web/src/components/Buttons/SortQuesTabGroup.tsx
+++ packages/web/src/components/Buttons/SortQuesTabGroup.tsx
@@ -2,6 +2,7 @@ import 'twin.macro'
 import React from 'react'
 import { TabGroupItem, TabGroup } from '../my-mui/Misc'
 import { QuestionSortBy } from '~~/generated/graphql'
+import Link from 'next/link'
 
 interface SortQuesBarProps {
   sortBy: QuestionSortBy
@@ -11,36 +12,34 @@ const SortQuesBar = ({ sortBy }: SortQuesBarProps) => {
   return (
     <div tw="flex justify-end my-4 text-sm sm:text-base">
       <TabGroup>
-        <TabGroupItem
-          active={sortBy === QuestionSortBy.Hot}
-          href={`/?tab=${QuestionSortBy.Hot}`}
-        >
-          Hot
-        </TabGroupItem>
-        <TabGroupItem
-          active={sortBy === QuestionSortBy.Votes}
-          href={`/?tab=${QuestionSortBy.Votes}`}
-        >
-          Votes
-        </TabGroupItem>
-        <TabGroupItem
-          active={sortBy === QuestionSortBy.Views}
-          href={`/?tab=${QuestionSortBy.Views}`}
-        >
-          Views
-        </TabGroupItem>
-        <TabGroupItem
-          active={sortBy === QuestionSortBy.Newest}
-          href={`/?tab=${QuestionSortBy.Newest}`}
-        >
-          Newest
-        </TabGroupItem>
-        <TabGroupItem
-          active={sortBy === QuestionSortBy.Oldest}
-          href={`/?tab=${QuestionSortBy.Oldest}`}
-        >
-          Oldest
-        </TabGroupItem>
+        <Link href={`/?tab=${QuestionSortBy.Hot}`} passHref>
+          <TabGroupItem active={sortBy === QuestionSortBy.Hot}>
+            Hot
+          </TabGroupItem>
+        </Link>
+        <Link href={`/?tab=${QuestionSortBy.Votes}`} passHref>
+          <TabGroupItem active={sortBy === QuestionSortBy.Votes}>
+            Votes
+          </TabGroupItem>
+        </Link>
+        <Link passHref href={`/?tab=${QuestionSortBy.Views}`}>
+          <TabGroupItem
+            active={sortBy === QuestionSortBy.Views}
+            href={`/?tab=${QuestionSortBy.Views}`}
+          >
+            Views
+          </TabGroupItem>
+        </Link>
+        <Link href={`/?tab=${QuestionSortBy.Newest}`} passHref>
+          <TabGroupItem active={sortBy === QuestionSortBy.Newest}>
+            Newest
+          </TabGroupItem>
+        </Link>
+        <Link href={`/?tab=${QuestionSortBy.Oldest}`} passHref>
+          <TabGroupItem active={sortBy === QuestionSortBy.Oldest}>
+            Oldest
+          </TabGroupItem>
+        </Link>
       </TabGroup>
     </div>
   )
